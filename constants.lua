fa6a981b5d7e63792e20817d6059ca6b3e077d9b
Vadim1987 <vadim.stetskiy@gmail.com>
2025-07-20 15:23:18 +0200

    Create constants.lua

---
+++ constants.lua
@@ -0,0 +1,26 @@
+-- All constants in one place
+
+-- Window dimensions
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 600
+
+-- Paddle settings
+PADDLE_WIDTH = 10
+PADDLE_HEIGHT = 60
+PADDLE_SPEED = 300
+
+-- Ball settings
+BALL_SIZE = 10
+BALL_SPEED_X = 240
+BALL_SPEED_Y = 120
+
+-- Score limit
+WIN_SCORE = 10
+
+-- Positioning
+PADDLE_OFFSET_X = 30
+SCORE_OFFSET_Y = 40
+
+-- Colors
+COLOR_BG = {0, 0, 0}
+COLOR_FG = {1, 1, 1}
