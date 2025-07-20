b03a3ebcf7fff2fc8156aea45ab5d2721b60cdd2
Vadim1987 <vadim.stetskiy@gmail.com>
2025-07-20 20:46:12 +0200

    Implement ball modules for first-person field
    Ball tracks previous frame for swept collisions; centered spawn.

--- ball.lua
+++ ball.lua
@@ -1,23 +1,25 @@
+-- Ball / puck with previous position for swept collision
+
 local Ball = {}
 Ball.__index = Ball
 
-function Ball:create()
-    local ball = setmetatable({}, self)
-    ball.radius = BALL_SIZE
-    ball.x = (BAT_MIN_X + BAT_MAX_X) / 2
-    ball.y = (BAT_MIN_Y + BAT_MAX_Y) / 2
-    ball.dx = BALL_SPEED_X * (math.random(2) == 1 and 1 or -1)
-    ball.dy = BALL_SPEED_Y * (math.random(2) == 1 and 1 or -1)
-    ball.prev_x = ball.x
-    ball.prev_y = ball.y
-    return ball
+function Ball:new()
+    local o = setmetatable({}, self)
+    o.radius = BALL_RADIUS
+    o.x = (ZONE_MIN_X + ZONE_MAX_X) / 2
+    o.y = (ZONE_MIN_Y + ZONE_MAX_Y) / 2
+    o.dx = BALL_SPEED_X * (love.math.random(2) == 1 and 1 or -1)
+    o.dy = BALL_SPEED_Y * (love.math.random(2) == 1 and 1 or -1)
+    o.prev_x = o.x
+    o.prev_y = o.y
+    return o
 end
 
 function Ball:reset()
-    self.x = (BAT_MIN_X + BAT_MAX_X) / 2
-    self.y = (BAT_MIN_Y + BAT_MAX_Y) / 2
-    self.dx = BALL_SPEED_X * (math.random(2) == 1 and 1 or -1)
-    self.dy = BALL_SPEED_Y * (math.random(2) == 1 and 1 or -1)
+    self.x = (ZONE_MIN_X + ZONE_MAX_X) / 2
+    self.y = (ZONE_MIN_Y + ZONE_MAX_Y) / 2
+    self.dx = BALL_SPEED_X * (love.math.random(2) == 1 and 1 or -1)
+    self.dy = BALL_SPEED_Y * (love.math.random(2) == 1 and 1 or -1)
     self.prev_x = self.x
     self.prev_y = self.y
 end
@@ -30,6 +32,9 @@ function Ball:update(dt)
 end
 
 return Ball
+
+  
+
 
   
 
