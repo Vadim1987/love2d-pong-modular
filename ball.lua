b79fe446ad697ffed6b406b965a6d571466f15da
Vadim1987 <vadim.stetskiy@gmail.com>
2025-07-21 11:54:42 +0200

    implement player paddle and puck modules with swept collision support

--- ball.lua
+++ ball.lua
@@ -1,17 +1,14 @@
--- Ball / puck with previous position for swept collision
-
 local Ball = {}
 Ball.__index = Ball
 
 function Ball:new()
-    local o = setmetatable({}, self)
+    local o = setmetatable({}, Ball)
     o.radius = BALL_RADIUS
     o.x = (ZONE_MIN_X + ZONE_MAX_X) / 2
     o.y = (ZONE_MIN_Y + ZONE_MAX_Y) / 2
     o.dx = BALL_SPEED_X * (love.math.random(2) == 1 and 1 or -1)
     o.dy = BALL_SPEED_Y * (love.math.random(2) == 1 and 1 or -1)
-    o.prev_x = o.x
-    o.prev_y = o.y
+    o.prev_x, o.prev_y = o.x, o.y
     return o
 end
 
@@ -20,21 +17,18 @@ function Ball:reset()
     self.y = (ZONE_MIN_Y + ZONE_MAX_Y) / 2
     self.dx = BALL_SPEED_X * (love.math.random(2) == 1 and 1 or -1)
     self.dy = BALL_SPEED_Y * (love.math.random(2) == 1 and 1 or -1)
-    self.prev_x = self.x
-    self.prev_y = self.y
+    self.prev_x, self.prev_y = self.x, self.y
 end
 
 function Ball:update(dt)
-    self.prev_x = self.x
-    self.prev_y = self.y
+    self.prev_x, self.prev_y = self.x, self.y
     self.x = self.x + self.dx * dt
     self.y = self.y + self.dy * dt
 end
 
 return Ball
 
-  
-
+   
 
   
 
